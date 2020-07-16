0288f6af9f5b56c7a7e1d3b3a9b63ed3cb69c41c
Ефимов Александр <a.efimov@inenergy.ru>
2020-07-16 14:12:02 +0300

    update installation script

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 # MAIN APP INSTALLATION
-npm i
-npm run build
+npm i &&
+npm run build &&
 
 # MAIN APP AUTOSTART
 sudo mkdir /usr/share/bat
@@ -8,39 +8,4 @@ sudo cp -rf dist/linux-armv7l-unpacked/** /usr/share/bat/
 echo '/usr/share/bat/bat-ui' > ~/.xinitrc
 chmod +x ~/.xinitrc
 
-
-# BOOT SPEED OPTIMIZATIONS
-cat <<EOT >> /boot/config.txt
-# Disable bluetooth
-dtoverlay=pi3-disable-bt
-
-#Disable Wifi
-dtoverlay=pi3-disable-wifi
- 
-# Overclock the SD Card from 50 to 100MHz
-# This can only be done with at least a UHS Class 1 card
-dtoverlay=sdtweak,overclock_50=100
- 
-# Set the bootloader delay to 0 seconds. The default is 1s if not specified.
-boot_delay=0
-
-# Overclock the raspberry pi. This voids its warranty. Make sure you have a good power supply.
-force_turbo=1
-EOT
-
-echo "quiet" >> /boot/cmdline.txt
-
-systemctl disable dhcpcd.service
-systemctl disable networking.service
-systemctl disable ssh.service
-systemctl disable ntp.service
-systemctl disable dphys-swapfile.service
-systemctl disable keyboard-setup.service
-systemctl disable apt-daily.service
-systemctl disable wifi-country.service
-systemctl disable hciuart.service
-systemctl disable raspi-config.service
-systemctl disable avahi-daemon.service
-systemctl disable triggerhappy.service
-
-apt-get purge --remove plymouth
+while read p; do sudo systemctl disable "$p"; done < unused-packages.list
